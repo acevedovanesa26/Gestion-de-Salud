1949b4dc511032f4758c76d5b73997221703cef0
Vanesa <vacevedom1@ucentral.edu.co>
2025-03-31 19:56:51 -0500

    Initial commit con la estructura del proyecto Spring Boot

---
+++ src/main/java/co/edu/ucentral/gestiondesalud/GestionDeSaludApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.gestiondesalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeSaludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ucentral/gestiondesalud/GestionDeSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucentral.gestiondesalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
